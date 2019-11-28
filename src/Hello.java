6b5b3ebf1a4138d7a7c50f2bfd752428c6de9a5b
joetta25 <joetta.chasidy@gmail.com>
2019-11-28 11:07:10 -0500

    starting off with expressions

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+        int myFirstNumber = (2 + 5) + (8 * 15);
+        int mySecondNumber = myFirstNumber * 2;
+        int myThirdNumber = 6;
+        int myTotal = mySecondNumber + myThirdNumber + myFirstNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+    }
+}
