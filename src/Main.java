4bd5b1fd813b82d7c2a25141cf3ffaea941bd17f
joetta25 <joetta.chasidy@gmail.com>
2019-11-28 11:40:57 -0500

    learned primitive types and Wrapper Class

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        int myMinValue = Integer.MIN_VALUE;
+        int myMaxValue = Integer.MAX_VALUE;
+
+        System.out.println("My max value = " + myMaxValue);
+        System.out.println("My min value = " + myMinValue );
+
+    }
+}
