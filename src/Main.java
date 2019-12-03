83cdad390c05ace875877701d117b97a56cc53bb
joetta25 <joetta.chasidy@gmail.com>
2019-12-02 22:09:03 -0500

    learned if-then , logical and Operators, and the OR operator

--- src/Main.java
+++ src/Main.java
@@ -13,23 +13,45 @@ public class Main {
 //
 //        System.out.println(myFirstCharValue);
 //        System.out.println(mySecondCharValue);
+//
+//        String myString = " hello my name is  joetta";
+//        myString = myString + " I am a software developer";
+//        System.out.println(" my string is equal to " + myString);
+//        myString = myString + " \u00A9 2019";
+//        System.out.println(" my string is equal to " + myString);
+//        String myStringNumber = " 244.95";
+//        myStringNumber = myStringNumber + " 764.900";
+//        System.out.println(myStringNumber);
+//        String lastString = "10";
+//        int myInt = 50;
+//        lastString = lastString + myInt;
+//        System.out.println("LastString is equal to " + lastString);
+//        double doubleNumber = 120.47d;
+//        lastString = lastString + doubleNumber;
+//
+//        //----if-then statements
 
-        String myString = " hello my name is  joetta";
-        myString = myString + " I am a software developer";
-        System.out.println(" my string is equal to " + myString);
-        myString = myString + " \u00A9 2019";
-        System.out.println(" my string is equal to " + myString);
-        String myStringNumber = " 244.95";
-        myStringNumber = myStringNumber + " 764.900";
-        System.out.println(myStringNumber);
-        String lastString = "10";
-        int myInt = 50;
-        lastString = lastString + myInt;
-        System.out.println("LastString is equal to " + lastString);
-        double doubleNumber = 120.47d;
-        lastString = lastString + doubleNumber;
 
-        System.out.println(" Laststring is equal to " + lastString);
+        boolean isAlien = false;
 
+        if( isAlien == false){
+
+            System.out.println( "Its not an alien");
+
+        }
+
+        int topScore = 100;
+        if (topScore < 100 ) {
+            System.out.println("Top score is less than 100");
+        }
+
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90 ) ) {
+            System.out.println(" it works");
+        }
     }
 }
